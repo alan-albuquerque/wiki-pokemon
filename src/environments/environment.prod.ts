717c889ecf1c36f468f79b33be0806b026872769
Alan Albuquerque <alan@paggtaxi.com.br>
2018-08-12 23:26:48 -0300

    colocando header na tabela de pokemons com titulos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'https://localhost:8000',
   defaultLanguage: 'pt-BR',
   supportedLanguages: [
     'pt-BR',
